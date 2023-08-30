cca28e4a87503f3f2faf15594701fd69389e28a1
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-08-30 09:12:46 +0200

    Add accident location, lock map extent, remove default action

--- gameModel/libs/ClientScript/game/mainSimulationLogic.ts
+++ gameModel/libs/ClientScript/game/mainSimulationLogic.ts
@@ -3,12 +3,12 @@
  */
 
 import { mainSimLogger } from "../tools/logger";
-import { GetInformationAction } from "./common/actions/actionBase";
 import { ActionTemplateBase, DefineMapObjectTemplate, GetInformationTemplate } from "./common/actions/actionTemplateBase";
 import { Actor } from "./common/actors/actor";
 import { ActorId, TemplateRef } from "./common/baseTypes";
 import { TimeSliceDuration } from "./common/constants";
 import { initBaseEvent } from "./common/events/baseEvent";
+import { MapFeature } from "./common/events/defineMapObjectEvent";
 import { ActionCreationEvent, TimeForwardEvent, TimedEventPayload } from "./common/events/eventTypes";
 import { compareTimedEvents, FullEvent, getAllEvents, sendEvent } from "./common/events/eventUtils";
 import { TimeForwardLocalEvent } from "./common/localEvents/localEventBase";
@@ -49,12 +49,17 @@ function initMainState(): MainSimulationState {
   const testMCS = new Actor('MCS', 'actor-mcs', 'actor-mcs-long')
   const testACS = new Actor('ACS', 'actor-als', 'actor-als-long')
 
-  const testAction = new GetInformationAction(0, TimeSliceDuration * 2, 'message-key', 'action name', 0, testAL.Uid);
+  const mainAccident: MapFeature = {
+    type: 'Point',
+    name: 'mainAccident',
+    geometry: [2497449.9236694486,1120779.3310497932]
+  }
+
 
   return new MainSimulationState({
-    actions: [testAction],
+    actions: [],
     actors: [testAL, testMCS, testACS],
-    mapLocations: [],
+    mapLocations: [mainAccident],
     patients: [],
     tasks: [],
     radioMessages: []
