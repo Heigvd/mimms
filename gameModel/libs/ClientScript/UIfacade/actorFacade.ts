f86f0dcedec8f56c037beee053dbee05d790f47b
Xavier Good <71324808+xgo-work@users.noreply.github.com>
2023-11-15 10:11:42 +0100

    added trainer page to show end game button. Fixed send resources drop down bug

--- gameModel/libs/ClientScript/UIfacade/actorFacade.ts
+++ gameModel/libs/ClientScript/UIfacade/actorFacade.ts
@@ -1,4 +1,5 @@
 import { Actor } from "../game/common/actors/actor";
+import { ActorId } from "../game/common/baseTypes";
 import { getCurrentState } from "../game/mainSimulationLogic";
 
 
@@ -15,3 +16,30 @@ export function getAllActors(): Readonly<Actor[]> {
 export function getActor(id: number): Readonly<Actor | undefined> | undefined {
 	return getCurrentState().getActorById(id);
 }
+
+export function getAnyOtherActorId(actorId: ActorId) : ActorId {
+	const all = getAllActors();
+	
+	const allBut = all.filter(a => a !== getActor(actorId));
+	if(allBut.length == 0){ // no other actor
+		//fallback on current
+		return actorId;
+	}
+	return allBut[0].Uid; // take any other in the list
+}
+
+/**
+ * Returns an actor id that is not currently selected in the timeline.
+ * falls back on current actor if the only one
+ * @param main simulation Context
+ */
+export function getOtherValidActor(ctx: any) : ActorId {
+	const resSelected = ctx.interfaceState.state.resources.sendResources.selectedActorId;
+	const timelineSelected = ctx.interfaceState.state.currentActorUid;
+	if(resSelected == timelineSelected){
+		return getAnyOtherActorId(Context.interfaceState.state.resources.sendResources.selectedActorId);
+	}
+	return Context.interfaceState.state.resources.sendResources.selectedActorId;
+
+}
+
