d00388d8a863822a8806d43aab831e9a96bc994c
Jess <47330627+so-yhi@users.noreply.github.com>
2024-05-29 17:01:08 +0200

    WIP - add modal for -waiting room- in multiplayer

--- gameModel/libs/ClientScript/UIfacade/patientFacade.ts
+++ gameModel/libs/ClientScript/UIfacade/patientFacade.ts
@@ -11,3 +11,16 @@ export function getAllPatients(): Readonly<PatientState[]> {
 export function getPatient(id: string): Readonly<PatientState | undefined> {
   return getAllPatients().find(patient => patient.patientId === id);
 }
+
+/* old hack - to be updated
+export function keepStateAlive({ state, setState }: FullState) {
+	const ePatient = getCurrentPatientId();
+	const cPatient = state.currentPatient;
+	if (ePatient !== cPatient) {
+		setState({
+			...getInitialPatientZoomState(),
+			currentPatient: ePatient,
+		});
+	}
+}
+*/
