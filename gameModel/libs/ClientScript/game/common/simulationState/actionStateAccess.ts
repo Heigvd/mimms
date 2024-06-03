8a3f03955518692b801b5c84656140653ee9b2e5
Fabio Santaniello <fabio.santaniello@heig-vd.ch>
2024-06-03 15:35:01 +0200

    MIM-223, MIM-139: reusing getOngoingActions

--- gameModel/libs/ClientScript/game/common/simulationState/actionStateAccess.ts
+++ gameModel/libs/ClientScript/game/common/simulationState/actionStateAccess.ts
@@ -5,9 +5,8 @@ export function getOngoingActionsForActor(
   state: Readonly<MainSimulationState>,
   actorUid: number
 ): ActionBase[] {
-  return state
-    .getAllActions()
-    .filter((a: ActionBase) => a.ownerId === actorUid && a.getStatus() === 'OnGoing');
+  return getOngoingActions(state)
+    .filter((a: ActionBase) => a.ownerId === actorUid);
 }
 
 export function getOngoingActions(state: Readonly<MainSimulationState>): ActionBase[] {
