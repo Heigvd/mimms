03fdd1e433653d903bb7bec6e386c1e69d93b162
Jess <47330627+so-yhi@users.noreply.github.com>
2024-06-03 10:34:44 +0200

    WIP - EVASAN form structure

---
+++ gameModel/libs/ClientScript/UIfacade/evasanFacade.ts
@@ -0,0 +1,9 @@
+export function isChoiceOpen() {
+  let isChoiceOpen = Boolean;
+  return isChoiceOpen;
+}
+
+export function isChoiceFilledAndClosed() {
+  let isChoiceFilledAndClosed = Boolean;
+  return isChoiceFilledAndClosed;
+}
