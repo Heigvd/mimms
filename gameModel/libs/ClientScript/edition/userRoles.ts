7d35f9681470bb8b03e8fff8241c6d797a8d5024
Xavier Good <71324808+xgo-work@users.noreply.github.com>
2025-04-09 17:13:46 +0200

    default to pretri mode. Added MIMMS role to view only patient generation.

---
+++ gameModel/libs/ClientScript/edition/userRoles.ts
@@ -0,0 +1,14 @@
+Roles.setRoles(
+  {
+    MIMMS: {
+      id: 'MIMMS',
+      availableTabs: ['Patient Generation'],
+      label: {
+        EN: 'MIMMS',
+        FR: 'MIMMS',
+      },
+    },
+  },
+  'MIMMS',
+  'MIMMS_ROLES'
+);
