2e403f5dc84871d4aa7253d82cf86f2fc7b11a94
xgo-work <71324808+xgo-work@users.noreply.github.com>
2025-08-27 10:15:50 +0200

    Made corrections accessible at the end of exam mode. improved end page structure, removed unnessary conditions

--- gameModel/libs/ClientScript/UIfacade/pretriFacade.ts
+++ gameModel/libs/ClientScript/UIfacade/pretriFacade.ts
@@ -1,5 +1,8 @@
+import { getCategorizedHumans } from '../game/legacy/the_world';
+import { getDrillStatus } from '../game/pretri/drill';
 import { toHoursMinutesSecondsIso } from '../tools/helper';
 import { getTranslation } from '../tools/translation';
+import { getSortedPatientIds } from '../tools/WegasHelper';
 
 /**
  * Returns the latest stored pretri time in drill games
@@ -9,4 +12,29 @@ export function getFormattedPretriTime(): string {
   const time = toHoursMinutesSecondsIso(t);
   const timeLabel = getTranslation('pretriage-interface', 'time');
   return `${timeLabel} ${time}`;
+}
+
+export function actionsBlocked(): boolean {
+  return getDrillStatus() !== 'ongoing' && Variable.find(gameModel, 'examMode').getValue(self);
+}
+
+/**
+ * Returns a formatted version of the computed total number of patients
+ */
+export function getPatientPretriTotal(): string {
+  const textPatientCorrect = getTranslation('pretriage-interface', 'nb-patients-correct');
+  const sortedId = getSortedPatientIds();
+  const allHumans = getCategorizedHumans();
+  let nbCorrect = 0;
+  const r = sortedId
+    .map(id => {
+      return allHumans.find(h => h.id === id);
+    })
+    .flatMap(p => (p ? [p] : []));
+  r.forEach(pat => {
+    if (pat.categorization?.category == pat.categorization?.autoTriage.categoryId) {
+      nbCorrect++;
+    }
+  });
+  return `${nbCorrect}/${r.length} ${textPatientCorrect}`;
 }
