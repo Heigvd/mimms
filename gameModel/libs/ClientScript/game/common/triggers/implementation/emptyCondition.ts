582886b5d5dfcdd87587c21505a5723c842e9fe2
xgo-work <71324808+xgo-work@users.noreply.github.com>
2025-09-17 11:51:32 +0200

    Update gameModel/libs/ClientScript/game/common/triggers/implementation/emptyCondition.ts
    Co-authored-by: Sandra Monnier <70577952+SandraMonnier@users.noreply.github.com>

--- gameModel/libs/ClientScript/game/common/triggers/implementation/emptyCondition.ts
+++ gameModel/libs/ClientScript/game/common/triggers/implementation/emptyCondition.ts
@@ -2,7 +2,7 @@ import { IDescriptor, Indexed, Typed } from '../../interfaces';
 
 /**
  * Condition that does nothing, meaning it should neither return true neither false
- * Has to be filtered out
+ * Has to be filtered out, used for scenarist interface purposes
  */
 export interface EmptyCondition extends IDescriptor, Typed, Indexed {
   type: 'empty';
