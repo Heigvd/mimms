8bd78d0ca941839525f7f71e51b5a845d84a9e55
jeanne <180178277+jeanneweb@users.noreply.github.com>
2025-12-03 16:52:47 +0100

    new file and tools customization

--- gameModel/libs/ClientScript/edition/UIfacade/map/styling.ts
+++ gameModel/libs/ClientScript/edition/UIfacade/map/styling.ts
@@ -18,6 +18,37 @@ export function getScenaristLayerStyle(feature: any): LayerStyleObject {
 
 function getFeatureColor(feature: any): string {
   // TODO amazing stuff for color
-
   return DEFAULT_COLOR;
+}
+
+export function getDrawStyle(_feature: any): LayerStyleObject {
+  const strokeStyle: StrokeStyleObject = {
+    type: 'StrokeStyle',
+    color: '#3CA3CC',
+    width: 3,
+    lineCap: 'round',
+    lineJoin: 'round',
+  };
+
+  const fill: FillStyleObject = {
+    type: 'FillStyle',
+    color: '#3CA3CC' + '50',
+  };
+
+  const circleStyle: CircleStyleObject = {
+    type: 'CircleStyle',
+    fill: {
+      type: 'FillStyle',
+      color: '#3CA3CC',
+    },
+    stroke: {
+      type: 'StrokeStyle',
+      color: '#FFFFFF',
+      width: 2,
+    },
+    radius: 4,
+    // opacity: 1, TODO Not working for some odd reason
+  };
+
+  return { stroke: strokeStyle, fill, image: circleStyle };
 }
