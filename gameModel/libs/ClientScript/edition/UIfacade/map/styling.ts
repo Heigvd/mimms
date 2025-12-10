f331594329d2d38946c0d07eedf8bc93e1b31db4
jeanne <180178277+jeanneweb@users.noreply.github.com>
2025-12-10 10:26:59 +0100

    removed comments

--- gameModel/libs/ClientScript/edition/UIfacade/map/styling.ts
+++ gameModel/libs/ClientScript/edition/UIfacade/map/styling.ts
@@ -25,37 +25,35 @@ function getFeatureColor(feature: any): MapColorConfig {
       opacity = 1;
     }
   }
-  // TODO amazing stuff for color
   return { color: currentColor, opacity: opacity };
 }
 
 export function getDrawStyle(_feature: any): LayerStyleObject {
   const strokeStyle: StrokeStyleObject = {
     type: 'StrokeStyle',
-    color: '#3CA3CC', // change la couleur de la ligne du polygone/ligne pendant le dessin
-    width: 3, // change la largeur de la ligne du polygone/ligne pendant le dessin
+    color: '#3CA3CC',
+    width: 3,
     lineCap: 'round',
     lineJoin: 'round',
   };
 
   const fill: FillStyleObject = {
     type: 'FillStyle',
-    color: '#3CA3CC' + '50', // change la couleur en transparence à l'intérieur du polygone pendant le dessin
+    color: '#3CA3CC' + '50',
   };
 
   const circleStyle: CircleStyleObject = {
     type: 'CircleStyle',
     fill: {
       type: 'FillStyle',
-      color: '#3CA3CC', // change la couleur à l'intérieur du point pendant le dessin
+      color: '#3CA3CC',
     },
     stroke: {
       type: 'StrokeStyle',
-      color: '#FFFFFF', // change la couleur du contour du point pendant le dessin
-      width: 1.5, // change la largeur du contour du point pendant le dessin
+      color: '#FFFFFF',
+      width: 1.5,
     },
-    radius: 6, // change le rayon du point pendant le dessin
-    // opacity: 1, TODO Not working for some odd reason
+    radius: 6,
   };
 
   return { stroke: strokeStyle, fill, image: circleStyle };
