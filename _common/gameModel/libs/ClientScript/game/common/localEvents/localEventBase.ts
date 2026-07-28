98839da49fd5b9a86158a7b49417813e78de6fd9
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2026-07-28 17:27:27 +0200

    Small fix

--- _common/gameModel/libs/ClientScript/game/common/localEvents/localEventBase.ts
+++ _common/gameModel/libs/ClientScript/game/common/localEvents/localEventBase.ts
@@ -905,7 +905,7 @@ export class ReleaseResourcesFromTaskLocalEvent extends LocalEventBase {
       (resource: Resource) => resource.Uid
     );
     let location: LOCATION_ENUM = LOCATION_ENUM.entreeChantier;
-    if ((involvedResources[0].currentLocation = LOCATION_ENUM.PMA)) {
+    if ((involvedResources[0]?.currentLocation === LOCATION_ENUM.PMA)) {
       location = LOCATION_ENUM.PMA;
     }
 
